5b4df5c6364716714b9bb1fc0c79441d2fafa7e5
oskka521 <oskar.t.karlsson@gmail.com>
2020-07-04 16:52:44 +0200

    done for 2020704

--- code/main.py
+++ code/main.py
@@ -4,15 +4,35 @@ from PIL import ImageGrab
 import time
 
 
-print("start")
+def convert_to_gray(img):
+    processed_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    return(processed_img)
 
 
-last_time = time.time()
-while True:
+def extract_image():
     print_screen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
-    print(f'loop took {time.time()-last_time} sec')
-    cv2.imshow('window', np.array(print_screen_pil))
+    org_screen = np.array(print_screen_pil)
+    return(org_screen)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindow()
-        break
+
+def show_img(img):
+    cv2.imshow('window', img)
+
+
+def main():
+
+    once = 1
+    img = ""
+    while once == 1:
+        once = 0
+        img = extract_image()
+        gray_img = convert_to_gray(img)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+    print(gray_img)
+
+
+if __name__ == "__main__":
+    print("start")
+    main()
